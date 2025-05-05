6d1e2e2667e06a1786b766fcc9ded17f4f0b531c
jhonseloi <jhons.eloi@gmail.com>
2025-05-05 18:18:56 -0300

    atualizacao

--- curso-udemy/01-ts-intro/eslint.config.mjs
+++ curso-udemy/01-ts-intro/eslint.config.mjs
@@ -1,30 +1,41 @@
-import js from "@eslint/js";
-import tsPlugin from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
-// import prettierPlugin from "eslint-plugin-prettier"
+import js from '@eslint/js'
+import tsPlugin from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
+import prettierPlugin from 'eslint-plugin-prettier'
 
 export default [
-  js.configs.recommended,
+    js.configs.recommended,
 
-  {
-    files: ["**/*.ts"],
-    languageOptions: {
-      parser: tsParser,
-      ecmaVersion: 2020,
-      sourceType: "module",
-      globals: {
-        Atomics: "readonly",
-        SharedArrayBuffer: "readonly",
-      },
+    {
+        files: ['**/*.ts'],
+        languageOptions: {
+            parser: tsParser,
+            ecmaVersion: 2020,
+            sourceType: 'module',
+            globals: {
+                Atomics: 'readonly',
+                SharedArrayBuffer: 'readonly',
+            },
+        },
+        plugins: {
+            '@typescript-eslint': tsPlugin,
+            prettier: prettierPlugin,
+        },
+        rules: {
+            ...tsPlugin.configs['eslint-recommended'].overrides[0].rules,
+            ...tsPlugin.configs.recommended.rules,
+            ...prettierPlugin.configs.recommended.rules,
+            quotes: ['error', 'single'],
+            'prettier/prettier': [
+                'error',
+                {
+                    semi: false,
+                    trailingComma: 'all',
+                    singleQuote: true,
+                    printWidth: 80,
+                    tabWidth: 4,
+                },
+            ],
+        },
     },
-    plugins: {
-      "@typescript-eslint": tsPlugin,
-      // prettier: prettierPlugin,
-    },
-    rules: {
-      ...tsPlugin.configs["eslint-recommended"].overrides[0].rules,
-      ...tsPlugin.configs.recommended.rules,
-      // ...prettierPlugin.configs.recommended.rules,
-    },
-  },
-];
+]
