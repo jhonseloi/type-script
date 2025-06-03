b171a1265c2bb5236f2f076e7b1bd48ac36f21ae
jhonseloi <jhons.eloi@gmail.com>
2025-06-02 22:58:39 -0300

    atualizacao

---
+++ curso-udemy/02-ts-tipos-basicos/src/A009-tipo-enum/A009-tipo-enum.ts
@@ -0,0 +1,20 @@
+enum Cores {
+    Vermelho = 10, // 10
+    Azul = 100, // 100
+    Amarelo = 200, // 200
+}
+
+enum Cores {
+    Roxo = 'Roxo',
+    Verde = 201,
+    Rosa,
+}
+
+function escolhaACor(cor: Cores): void {
+    console.log(Cores[cor])
+}
+
+console.log(Cores)
+console.log(Cores[10])
+console.log(Cores.Vermelho)
+escolhaACor(Cores.Azul)
