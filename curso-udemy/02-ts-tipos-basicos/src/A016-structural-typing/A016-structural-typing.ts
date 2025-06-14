5c1eaaf1f5ee8d304b3cbde3c63d3a2c26b5b7ee
jhonseloi <jhons.eloi@gmail.com>
2025-06-14 17:04:06 -0300

    atualizacao

---
+++ curso-udemy/02-ts-tipos-basicos/src/A016-structural-typing/A016-structural-typing.ts
@@ -0,0 +1,14 @@
+type VerifyUserFunc = (user: User, sentValue: User) => boolean
+type User = { username: string; password: string }
+
+const verifyUser: VerifyUserFunc = (user, sentValue) => {
+    return (
+        user.username === sentValue.username &&
+        user.password === sentValue.password
+    )
+}
+
+const bdUser = { username: 'joao', password: '123456' }
+const sentUser = { username: 'joao', password: '123456', permissions: '' }
+const loggedIn = verifyUser(bdUser, sentUser)
+console.log(loggedIn)
