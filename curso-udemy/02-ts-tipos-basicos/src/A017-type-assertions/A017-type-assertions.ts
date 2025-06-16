6ff97e6e88c0f1d1c4f049e4c33bc320b04fddae
jhonseloi <jhons.eloi@gmail.com>
2025-06-16 16:16:16 -0300

    atualizacao

---
+++ curso-udemy/02-ts-tipos-basicos/src/A017-type-assertions/A017-type-assertions.ts
@@ -0,0 +1,23 @@
+/* RECOMENDADO */
+
+// Condicional
+const body1 = document.querySelector('body')
+if (body1) body1.style.background = 'red'
+
+// Type assertion
+const body2 = document.querySelector('body') as HTMLBodyElement
+body2.style.background = 'red'
+
+// HTML Element
+const input = document.querySelector('.input') as HTMLInputElement
+input.value = 'Qualquer coisa'
+input.focus()
+
+/* NÃO RECOMENDADO */
+
+// Non-null assertion (!)
+const body3 = document.querySelector('body')!
+body3.style.background = 'blue'
+
+// Type assertion
+const body4 = (document.querySelector('body') as unknown) as number
