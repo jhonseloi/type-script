149499f2e79eb9dfd052620efc92432f10dfd577
jhonseloi <jhons.eloi@gmail.com>
2025-09-22 18:51:16 -0300

    atualizacao

---
+++ curso-udemy/05-ts-tipos-genericos/src/A008-generics+utility-types/A008-generics+utility-types.ts
@@ -0,0 +1,50 @@
+// Record
+const pessoa1: Record<string, string | number> = {
+    nome: 'Erling',
+    sobrenome: 'Haaland',
+    idade: 25,
+}
+console.log(pessoa1)
+
+type Pessoa = {
+    nome?: string
+    sobrenome?: string
+    idade?: number
+}
+
+// Required
+type PessoaRequired = Required<Pessoa>
+const pessoa2: PessoaRequired = {
+    nome: 'Tijani',
+    sobrenome: 'Reijnders',
+    idade: 27,
+}
+console.log(pessoa2)
+
+// Partial
+type PessoaPartial = Partial<PessoaRequired>
+const pessoa3: PessoaPartial = {
+    nome: 'Bernardo',
+    sobrenome: 'Silva',
+}
+console.log(pessoa3)
+
+// Readonly
+type PessoaReadonly = Readonly<PessoaRequired>
+const pessoa4: PessoaReadonly = {
+    nome: 'Phil',
+    sobrenome: 'Foden',
+    idade: 25,
+}
+console.log(pessoa4)
+
+// Pick
+type PessoaPick = Pick<Pessoa, 'nome' | 'idade'>
+const pessoa5: PessoaPick = {
+    nome: 'Savinho',
+    idade: 21,
+}
+console.log(pessoa5)
+
+// module mode
+export default 1
