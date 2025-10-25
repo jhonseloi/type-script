f7a358ba453f00eec7e83c9652478933d6517048
jhonseloi <jhons.eloi@gmail.com>
2025-10-25 18:53:23 -0300

    atualizacao

---
+++ curso-udemy/08-ts-solid/src/ocp/main.ts
@@ -0,0 +1,26 @@
+// eslint-disable-next-line
+import { TenPercentDiscount, FiftyPercentDiscount, NoDiscount } from './entities/discount'
+import { ShoppingCart } from './entities/shopping-cart'
+import { Order } from './entities/order'
+import { Messaging } from './services/messaging'
+import { Persistency } from './services/persistency'
+import { Product } from './entities/product'
+
+// const tenPercentDiscount = new TenPercentDiscount()
+const fiftyPercentDiscount = new FiftyPercentDiscount()
+// const noDiscount = new NoDiscount()
+const shoppingCart = new ShoppingCart(fiftyPercentDiscount)
+const messaging = new Messaging()
+const persistency = new Persistency()
+const order = new Order(shoppingCart, messaging, persistency)
+
+shoppingCart.addItem(new Product('Camiseta', 49.9))
+shoppingCart.addItem(new Product('Caderno', 10.9))
+shoppingCart.addItem(new Product('Lápis', 1.49))
+
+console.log(shoppingCart.items)
+console.log(shoppingCart.total())
+console.log(shoppingCart.totalWithDicount())
+console.log(order.orderStatus)
+order.checkout()
+console.log(order.orderStatus)
