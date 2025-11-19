844a8c7def6c86d25694975eb191857bc9b71010
jhonseloi <jhons.eloi@gmail.com>
2025-11-18 22:49:02 -0300

    atualizacao

---
+++ curso-udemy/10-ts-pomodoro-react/eslint.config.mjs
@@ -0,0 +1,42 @@
+import js from '@eslint/js'
+import tsPlugin from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
+import prettierPlugin from 'eslint-plugin-prettier'
+import react from "eslint-plugin-react"
+
+export default [
+    js.configs.recommended,
+    react.configs.recommended,
+    {
+        files: ['**/*.ts'],
+        languageOptions: {
+            parser: tsParser,
+            ecmaVersion: 2020,
+            sourceType: 'module',
+            globals: {
+                Atomics: 'readonly',
+                SharedArrayBuffer: 'readonly',
+            },
+        },
+        plugins: {
+            '@typescript-eslint': tsPlugin,
+            prettier: prettierPlugin,
+        },
+        rules: {
+            ...tsPlugin.configs['eslint-recommended'].overrides[0].rules,
+            ...tsPlugin.configs.recommended.rules,
+            ...prettierPlugin.configs.recommended.rules,
+            quotes: ['error', 'single'],
+            'prettier/prettier': [
+                'error',
+                {
+                    semi: false,
+                    trailingComma: 'all',
+                    singleQuote: true,
+                    printWidth: 80,
+                    tabWidth: 4,
+                },
+            ],
+        },
+    },
+]
